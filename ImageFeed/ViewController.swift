1cec19841430852e530b0b55321ca10b5cdf6310
SuperSweetDainty <danya.laputin@mail.ru>
2025-06-14 23:01:15 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Даниил Лапутин on 14.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
